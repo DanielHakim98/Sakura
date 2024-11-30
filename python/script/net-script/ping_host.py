ea9b569676ef8ff1baa2bc65e6c34b381b6ea245
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-11-30 10:47:10 +0800

    python/script: modify net-script/ping_host.py

--- python/script/net-script/ping_host.py
+++ python/script/net-script/ping_host.py
@@ -1,10 +1,11 @@
+from collections.abc import Iterator
 import subprocess
 import argparse
 import ipaddress
 from concurrent.futures import ThreadPoolExecutor
 
 
-def ping_host(ip):
+def ping_host(ip: str) -> str | None:
     """
     Pings a single host and returns the IP if it is reachable.
     """
@@ -21,21 +22,21 @@ def ping_host(ip):
     return None
 
 
-def ping_subnet(subnet):
+def ping_subnet(subnet: str, worker: int) -> list[str]:
     """
     Pings all hosts within the given subnet and returns a list of reachable IPs.
     """
     reachable_hosts = []
     print("Pinging all hosts in the subnet. Please wait...")
 
-    with ThreadPoolExecutor(max_workers=50) as executor:
+    with ThreadPoolExecutor(max_workers=worker) as executor:
         results = executor.map(ping_host, generate_ips(subnet))
 
     reachable_hosts = [ip for ip in results if ip is not None]
     return reachable_hosts
 
 
-def generate_ips(cidr):
+def generate_ips(cidr: str) -> Iterator[str]:
     network = ipaddress.IPv4Network(cidr, strict=False)
     for ip in network.hosts():
         yield str(ip)
@@ -45,13 +46,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="ip-listener", description="simple multi-thread ping within subnet"
     )
-    parser.add_argument("-r", "--cidr")
-    parser.add_argument("-c", "--concurrent")
+    parser.add_argument("-c", "--cidr")
+    parser.add_argument("-w", "--worker", type=int, default=20)
     args = parser.parse_args()
 
     print("cidr:", args.cidr)
-    reachable = ping_subnet(args.cidr)
-
+    print("worker:", args.worker)
+    reachable_hosts = ping_subnet(args.cidr, args.worker)
     print("Reachable hosts:")
-    for host in reachable:
+    for host in reachable_hosts:
         print(host)
